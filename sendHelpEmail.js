d9464dd0e34576720fddff24499e251362a231a4
AdityaGautam1911 <adityagautam1911@gmail.com>
2024-11-13 16:03:59 +0530

    updated files for emergency system

--- sendHelpEmail.js
+++ sendHelpEmail.js
@@ -1,59 +1,4 @@
-// //NON REPETITIVE ENAIL FUCTIONALITY
-// const { SerialPort } = require("serialport");
-// const { ReadlineParser } = require("@serialport/parser-readline");
-// const nodemailer = require("nodemailer");
-
-// const port = new SerialPort({ path: "COM11", baudRate: 9600 });
-// const parser = port.pipe(new ReadlineParser({ delimiter: "\n" }));
-
-// let emailSent = false; // Flag to track if an email has been sent
-// const debounceTimeout = 5000; // 5 seconds timeout before allowing a new email
-
-// // Set up Nodemailer transporter
-// let transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: "adityagautam1911@gmail.com",
-//     pass: "yozf krye dxte pybs",
-//   },
-// });
-
-// // Function to send an email
-// function sendHelpEmail() {
-//   const mailOptions = {
-//     from: "adityagautam1911@gmail.com",
-//     to: "adityagautam1911@gmail.com",
-//     subject: "Help Alert!",
-//     text: "Help button has been pressed on the Arduino!",
-//   };
-
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       console.error("Error sending email:", error);
-//     } else {
-//       console.log("Email sent:", info.response);
-//     }
-//   });
-// }
-
-// // Listen for data from Arduino
-// parser.on("data", (data) => {
-//   console.log("Received data from Arduino:", data);
-
-//   if (data.trim() === "Help (this is test message)" && !emailSent) {
-//     console.log("Button pressed, sending help email...");
-//     sendHelpEmail();
-//     emailSent = true; // Set flag to prevent multiple emails
-
-//     // Reset the flag after debounceTimeout
-//     setTimeout(() => {
-//       emailSent = false;
-//       console.log("Ready to send a new email on next press.");
-//     }, debounceTimeout);
-//   }
-// });
-
-//EMAIL + WEBSITE ALERT
+// EMAIL + WEBSITE ALERT
 const express = require("express");
 const { SerialPort } = require("serialport");
 const { ReadlineParser } = require("@serialport/parser-readline");
@@ -75,7 +20,7 @@ let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "adityagautam1911@gmail.com",
-    pass: "", //put your app password here
+    pass: "", // replace with your app password
   },
 });
 
